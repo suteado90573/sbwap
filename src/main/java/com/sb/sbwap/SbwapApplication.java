f8e51b06ed9a59693f644e6a685bba17e79971d8
高木　大介 <takagi-d@iij.ad.jp>
2024-11-14 13:22:38 +0900

    #1 SpringInitializrで作成した資材を登録。

---
+++ src/main/java/com/sb/sbwap/SbwapApplication.java
@@ -0,0 +1,13 @@
+package com.sb.sbwap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/sbwap/SbwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sbwap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
