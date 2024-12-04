40da42d1a6afbb19458a0ef39c92b5a6d06ebfe9
高木　大介 <takagi-d@iij.ad.jp>
2024-12-04 10:22:39 +0900

    ログイン画面、トップ画面、エラー画面を追加。

---
+++ src/main/java/com/sb/sbwap/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.sb.sbwap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sb.sblib.service.AccountService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+	private final AccountService accountService;
+
+	@GetMapping("/login")
+	public String getLogin() {
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String postLogin(String mailAddress, String password) {
+
+		// 認証成功ならばトップ画面、認証失敗ならログイン画面に遷移する
+		if (accountService.auth(mailAddress, password)) {
+			return "top";
+		}
+		return "login";
+	}
+}
